2c54415aa2de509cb5f1341d062e5e1a160132ed
Ayush <indapureayush2003@gmail.com>
2025-03-27 14:34:31 +1100

    landing page commit

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 const express = require('express');
 const axios = require('axios');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware to parse JSON requests
+
 app.use(express.json());
 
-// Sample route
-app.get('/', (req, res) => {
-    res.send('Deakin University, Burood Campus');
-});
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
 
-// About Us route
 app.get('/about', (req, res) => {
     res.send('About Us: Deakin University is a public university in Victoria, Australia.');
 });
 
-// Add two numbers route
+
 app.get('/add', (req, res) => {
     const num1 = parseFloat(req.query.num1);
     const num2 = parseFloat(req.query.num2);
@@ -27,7 +26,6 @@ app.get('/add', (req, res) => {
     res.send(`The sum of ${num1} and ${num2} is ${sum}.`);
 });
 
-// Fetch user data from the API and handle errors
 app.get('/user/:id', async (req, res) => {
     const userId = req.params.id;
     try {
